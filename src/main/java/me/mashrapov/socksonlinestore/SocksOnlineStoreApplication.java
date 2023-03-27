7bd29442d837f4b1963c845b54a98282a8d853d6
b.Mashrapov <b_mashrapov@mail.ru>
2023-03-27 13:18:21 +0200

    Initial commit

---
+++ src/main/java/me/mashrapov/socksonlinestore/SocksOnlineStoreApplication.java
@@ -0,0 +1,13 @@
+package me.mashrapov.socksonlinestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocksOnlineStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocksOnlineStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/mashrapov/socksonlinestore/SocksOnlineStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package me.mashrapov.socksonlinestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocksOnlineStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
